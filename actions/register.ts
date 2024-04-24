00502c2e9d90534978764ffa87295a9fd9bd816e
Mariusz Michalski <supermariusz@gmail.com>
2024-04-24 11:27:22 +0200

    feat: create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 'use server';
 
+import bcrypt from 'bcrypt';
 import * as z from 'zod';
 
+import { getUserByEmail } from '@/data/user';
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -11,5 +14,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields!' };
   }
 
-  return { success: 'Email sent!' };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verification token email
+
+  return { success: 'User created!' };
 };
