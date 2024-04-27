4c48257c6a5f1f3b26b1d50f3cbcef178dfcce9e
Mariusz Michalski <supermariusz@gmail.com>
2024-04-27 16:36:32 +0200

    feat: enable oauth sign in (google/github)

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,16 +2,33 @@
 
 import { FaGithub } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
+import { signIn } from 'next-auth/react';
 
 import { Button } from '@/components/ui/button';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export const Social = () => {
+  const onClick = (provider: 'google' | 'github') => () => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
   return (
     <div className="flex w-full items-center gap-x-2">
-      <Button className="w-full" size="lg" variant="outline" onClick={() => {}}>
+      <Button
+        className="w-full"
+        size="lg"
+        variant="outline"
+        onClick={onClick('google')}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button className="w-full" size="lg" variant="outline" onClick={() => {}}>
+      <Button
+        className="w-full"
+        size="lg"
+        variant="outline"
+        onClick={onClick('github')}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
