c05a3ac372bbba6b87595bdd50ee346d6ba17522
Mariusz Michalski <supermariusz@gmail.com>
2024-05-06 23:19:04 +0200

    feat: send reset password email

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,11 +2,22 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+
+  await resend.emails.send({
+    from: 'onboarding@resend.dev', // TODO: replace with production email
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
+  });
+};
+
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'onboarding@resend.dev', // TODO: extract to a separate file
     to: email,
     subject: 'Confirm your email',
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
+import { getPasswordResetTokenByEmail } from '@/data/password-reset-token';
 import { getVerificationTokenByEmail } from '@/data/verification-token';
 import { db } from '@/lib/db';
 
@@ -7,6 +8,29 @@ import { db } from '@/lib/db';
  * Generate (any kind of) tokens and make sure that
  * if an existing token exists, it gets removed
  */
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(Date.now() + 3600 * 1000); // expire the token in 1 hour
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return passwordResetToken;
+};
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
