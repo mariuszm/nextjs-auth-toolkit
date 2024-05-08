401fbc79e3ca4a94f181ae1daf61633dca0d4625
Mariusz Michalski <supermariusz@gmail.com>
2024-05-08 12:17:21 +0200

    chore: example of protect server action with role gate

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -3,6 +3,7 @@
 import { UserRole } from '@prisma/client';
 import { toast } from 'sonner';
 
+import { admin } from '@/actions/admin';
 import { RoleGate } from '@/components/auth/role-gate';
 import { FormSuccess } from '@/components/form-success';
 import { Button } from '@/components/ui/button';
@@ -19,6 +20,18 @@ const AdminPage = () => {
     });
   };
 
+  const onServerActionClick = () => {
+    admin().then(data => {
+      if (data.error) {
+        toast.error(data.error);
+      }
+
+      if (data.success) {
+        toast.success(data.success);
+      }
+    });
+  };
+
   return (
     <Card className="w-[600px]">
       <CardHeader>
@@ -31,6 +44,11 @@ const AdminPage = () => {
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
           <p className="text-sm font-medium">Admin-only API Route</p>
           <Button onClick={onApiRouteClick}>Click to test</Button>
+        </div>
+
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-only Server Action</p>
+          <Button onClick={onServerActionClick}>Click to test</Button>
         </div>
       </CardContent>
     </Card>
