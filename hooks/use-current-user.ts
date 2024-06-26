cfed3d226a9a1490c6da7675ecce174c96c2d947
Mariusz Michalski <supermariusz@gmail.com>
2024-06-26 23:21:16 +0200

    chore: added comment

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -1,6 +1,10 @@
 import { useSession } from 'next-auth/react';
 
 export const useCurrentUser = () => {
+  /**
+   * TODO: Check with:
+   * const { data, status } = useSession({ required: true });
+   */
   const session = useSession();
 
   return session.data?.user;
